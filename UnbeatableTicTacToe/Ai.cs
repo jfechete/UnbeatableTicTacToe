11cf33226580653c842854053d9eb6a493cd40a6
jfechete <60266095+jfechete@users.noreply.github.com>
2021-11-03 22:53:57 -0700

    Created AI methods

--- UnbeatableTicTacToe/Ai.cs
+++ UnbeatableTicTacToe/Ai.cs
@@ -11,10 +11,12 @@ namespace UnbeatableTicTacToe
     {
         string _ai_file;
         dynamic _current_ai;
+        Random random;
 
         public Ai(string ai_file)
         {
             _ai_file = ai_file;
+            random = new Random();
             ResetAi();
         }
 
@@ -25,8 +27,29 @@ namespace UnbeatableTicTacToe
         public void ResetAi()
         {
             _current_ai = JsonConvert.DeserializeObject(System.IO.File.ReadAllText(_ai_file));
-            Console.Out.WriteLine(_current_ai.first.choices.Count);
-            Console.Out.WriteLine(_current_ai.first.choices[0].choice);
+        }
+
+        public void SetTurn(bool isComputer)
+        {
+            if (isComputer)
+            {
+                _current_ai = _current_ai.first;
+            }
+            else
+            {
+                _current_ai = _current_ai.second;
+            }
+        }
+
+        public void PlayerTurn(string position)
+        {
+            _current_ai = _current_ai.responses[position];
+        }
+
+        public string ComputerTurn()
+        {
+            _current_ai = _current_ai.choices[random.Next(_current_ai.choices.Count)];
+            return _current_ai.choice;
         }
     }
 }
